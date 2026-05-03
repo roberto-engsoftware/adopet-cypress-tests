28ca97fa26efc129a69fdc9649c4c2ce98f6313a
Roberto <roberto2006nascimento@gmail.com>
2026-05-03 15:55:36 -0300

    primeiro commit do projeto cypress

---
+++ cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,12 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Cadastrar').click();
+    cy.get('input[name = "nome"]').type('Joao Lucas');
+    cy.get('input[name = "email"]').type('joao.lucas@example.com');
+    cy.get('input[name = "password"]').type('Senha123');
+    cy.get('input[name = "confirm_password"]').type('Senha123');
+    cy.contains('button', 'Cadastrar').click();
+
+  })
+})
